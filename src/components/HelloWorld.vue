265af6b6192b1fe170e137ff14b35937cc8ade8f
owen.jackson@setvisions.co.uk <owen.jackson@setvisions.co.uk>
2023-08-17 00:40:32 +0100

    Added animation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,55 @@
 <script setup lang="ts">
+
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const jobs = ["Developer", "Dad", "3D Artist", "Tinkerer", "Team Leader"];
+
+const currentJob = ref(jobs[1]);
+
+function iterateJobs(): void {
+  let nextIndex = (jobs.indexOf(currentJob.value) + 1) % jobs.length;
+  currentJob.value = jobs[nextIndex];
+}
+
+let loopJobs;
+onMounted(() => {
+
+  loopJobs = setInterval(iterateJobs, 2000);
+
+});
+
+onUnmounted(() => clearInterval(loopJobs));
+
+
+
+
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="text-yellow-50 font-bold text-6xl font-sans"># Owen Jackson<br>## <span>Developer</span></h1>
-    <div class="h-4 w-full bg-yellow-400 mt-5"></div>
+  <div class="p-20 w-4/6">
+    <div>
+      <h1 id="title-text" class="text-yellow-50 font-bold text-8xl font-sans leading-relaxed h-24">I'm Owen Jackson,<br><span class="italic text-yellow-400">{{ currentJob }}</span></h1>
+      <!-- <div class="h-4 w-full bg-yellow-400 mt-5"></div> -->
+    </div>
   </div>
 </template>
+
+<style>
+@keyframes cursor-blink {
+  0% {
+    opacity: 0;
+  }
+}
+
+#title-text::after {
+  content: "";
+  position: relative;
+  left: 20px;
+  top: 10px;
+  width: 8px;
+  height: 100%;
+  background: rgb(250, 204, 21);
+  display: inline-block;
+  animation: cursor-blink 1s steps(2) infinite;
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,44 @@
 <script setup lang="ts">
+
+import { ref } from 'vue';
+
+const menuVisible = ref(false);
+
 </script>
 
 <template>
-    <div class="h-14 flex justify-between mt-4">
-        <div>
-            <h2 class=" font-bold text-2xl text-white ml-12">Owen Jackson</h2>
+
+    <nav class="fixed w-full z-20 top-0 left-0 bg-zinc-900">
+        <div class="flex flex-wrap mx-12 items-center justify-between p-4">
+                <span class="text-2xl font-semibold text-white">Owen Jackson</span>
+            <div class="">
+                <button @click="menuVisible=!menuVisible" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+            </div>
+            <div :class="{ hidden: !menuVisible }" class="items-center justify-between w-full md:flex md:w-auto md:order-1">
+                <ul class="flex flex-col p-4 md:p-0 mt-4 font-bold border md:flex-row md:space-x-8 md:mt-0 md:border-0 border-gray-700 rounded-lg">
+                    <li>
+                        <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:p-0 md:text-yellow-300">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 pl-3 pr-4 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 hover:text-blue-500 text-white hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent border-gray-700">About</a>
+                    </li> 
+                    <li> 
+                        <a href="#" class="block py-2 pl-3 pr-4 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 hover:text-blue-500 text-white hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent border-gray-700">Services</a>
+                    </li> 
+                    <li> 
+                        <a href="#" class="block py-2 pl-3 pr-4 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 hover:text-blue-500 text-white hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent border-gray-700">Contact</a>
+                    </li> 
+                    <li> 
+                        <a href="#" class="block py-2 pl-3 pr-4 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 hover:text-blue-500 text-white hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent border-gray-700">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div>
-            <ul class="font-bold text-2xl italic text-white mr-12">
-                <li class="inline mr-2 mb-2"><span class="border-b-4 border-yellow-400">Home</span> / </li>
-                <li class="inline mr-2">Work /</li>
-                <li class="inline mr-2">Contact /</li>
-                <li class="inline">Articles /</li>
-            </ul>
-        </div>
-    </div>
-  </template>
+    </nav>
+  
+        </template>
